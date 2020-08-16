ca36f08bc52fe343bf988098888ffb7595b54d8b
brlga002 <gabriel.lima1000@gmail.com>
2020-08-16 19:11:38 -0400

    Ajuste status code response

--- src/middleware/allowDigits.js
+++ src/middleware/allowDigits.js
@@ -6,13 +6,13 @@ module.exports = (request, response, next) => {
 
   if (fistNumberCheck.search(/[^0-1]/g) !== -1) {
     return response
-      .status(401)
+      .status(400)
       .json({ error: 'the fistNumber must be a binary' });
   }
 
   if (secondNumberCheck.search(/[^0-1]/g) !== -1) {
     return response
-      .status(401)
+      .status(400)
       .json({ error: 'the secondNumber must be a binary' });
   }
 
@@ -21,13 +21,13 @@ module.exports = (request, response, next) => {
 
   if (fistNumberDecimal > 255) {
     return response
-      .status(401)
+      .status(400)
       .json({ error: 'the fistNumber must be 0 to 255' });
   }
 
   if (secondNumberDecimal > 255) {
     return response
-      .status(401)
+      .status(400)
       .json({ error: 'the secondNumber must be 0 to 255' });
   }
 
