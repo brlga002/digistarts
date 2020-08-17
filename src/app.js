0ad9447ab75ab36fa3749e1deb40d043b53e6118
brlga002 <gabriel.lima1000@gmail.com>
2020-08-17 01:07:13 -0400

    stactic folder server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const mathRouter = require('./routes/mathRouter');
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(mathRouter);
 app.use(errors());
 
