b822bb0c831ab0a19dfad3daebaba0136e58162f
brlga002 <gabriel.lima1000@gmail.com>
2020-08-17 01:07:46 -0400

    Frontend app

---
+++ public/precache-manifest.0a3811c2f315c198584f5ef71fd01635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703eec2ded7a21c633bd5ed0e0930ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60eb8b493e6b43d5d59e",
+    "url": "/static/css/main.f80aad00.chunk.css"
+  },
+  {
+    "revision": "64e0ee739740aa938d73",
+    "url": "/static/js/2.2e055208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e055208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60eb8b493e6b43d5d59e",
+    "url": "/static/js/main.506898ab.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ public/precache-manifest.8db6b7bbc0242f279c82faff1f2e14b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc4af1803b6a2261ef72ce44c730b32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a9698199c352fca8d8",
+    "url": "/static/css/main.f80aad00.chunk.css"
+  },
+  {
+    "revision": "64e0ee739740aa938d73",
+    "url": "/static/js/2.2e055208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e055208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a9698199c352fca8d8",
+    "url": "/static/js/main.c33ce2a5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
